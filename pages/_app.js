4dba162753b14d35e8e1ee28fa9e14daaeff0536
kkukileon305 <kkukileon305@gmail.com>
2022-07-17 17:01:15 +0900

    초기화

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  body {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  #__next {
+    width: 540px;
+    height: 960px;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
